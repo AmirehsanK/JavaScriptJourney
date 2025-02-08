1cd8f40670f2e027ba573cb5227a51c94f04775e
Darkerys <amirehsank3@gmail.com>
2025-02-08 17:13:46 +0330

    Completed

--- SocialFeed/script.js
+++ SocialFeed/script.js
@@ -1,20 +1,27 @@
-//Global
+//#region Globals
 const deleteTimer = 2000;
 const maxChars = 150;
+const apiUrl = 'https://bytegrad.com/course-assets/js/1/api';
+//#endregion
 
+//#region Elements
 const textAreaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
 const feedEl = document.querySelector('.feedbacks');
 const submitEl = document.querySelector('.submit-btn');
+const hashtagListEl = document.querySelector('.hashtags');
+//#endregion
 
-//Handles how much character left for feedback input
+//#region Handles how much character left for feedback input
 const InputHandler = () =>{
     const charsNumber = textAreaEl.value.length;
     const charsLeft = maxChars - charsNumber;
     counterEl.innerText = charsLeft.toString();
 }
-//Function for handling form component
+//#endregion
+
+//#region Function for handling form component
 const SubmitHandler = (event) =>{
     event.preventDefault();
     const text =  textAreaEl.value;
@@ -38,11 +45,27 @@ const SubmitHandler = (event) =>{
         text: text
     };
     renderFeedbackItem(feed);
+    //Upload to api
+    fetch(apiUrl+'/feedbacks',{
+        method: 'POST',
+        body: JSON.stringify(feed),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }).then(response => {if(!response.ok)
+        console.log('error occurred');
+    else
+        console.log('Submitted Successfully');}
+    )
+
     textAreaEl.value = '';
     submitEl.blur();
     counterEl.textContent=maxChars.toString();
 }
+//#endregion
 
+//#region Form visual indicator
 const showVisualIndicator = (validator)=>
 {
     if (validator === true)
@@ -56,7 +79,9 @@ const showVisualIndicator = (validator)=>
         setTimeout(() => formEl.classList.remove('form--invalid'), deleteTimer);
     }
 }
+//#endregion
 
+//#region Render feedback items
 const renderFeedbackItem = feedback => {
     const feedItem = `
     <li class="feedback">
@@ -78,12 +103,50 @@ const renderFeedbackItem = feedback => {
     `;
     feedEl.insertAdjacentHTML('beforeend', feedItem);
 }
+//#endregion
 
+//#region ClickHandler
+const ClickHandler = (event) =>
+{
+    const clickedEl= event.target;
+    const upvoteEl = clickedEl.className.includes( 'upvote' );
+    if (upvoteEl)
+    {
+        const upvoteBtnEl = clickedEl.closest('.upvote');
+        upvoteBtnEl.disabled = true;
+        //this is only adding an upvote locally
+        const upvoteCountEl = upvoteBtnEl.querySelector('.upvote__count');
+        let upvoteCount = +upvoteCountEl.textContent;
+        upvoteCountEl.textContent = (++upvoteCount).toString();
+    }
+    else
+    {
+        clickedEl.closest('.feedback').classList.add('feedback--expand');
+    }
+}
+//#endregion
 
-textAreaEl.addEventListener('input', InputHandler)
-formEl.addEventListener('submit', SubmitHandler)
+//#region HashtagClickHandler
+const HashtagClickHandler= (event) =>
+{
+    const clickedEl= event.target;
+    if(clickedEl.className==='hashtags')
+        return;
+    const hashtagCompanyName = clickedEl.textContent.substring(1).trim().toLowerCase();
+    feedEl.childNodes.forEach(childNode =>
+    {
+        if (childNode.nodeType === 3)return;
+        const companyName = childNode.querySelector('.feedback__company').textContent.toLowerCase().trim();
+        if (hashtagCompanyName !== companyName)
+        {
+            childNode.remove();
+        }
+    })
+}
+//#endregion
 
-fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks').
+//#region Get feedbacks from server
+fetch(apiUrl+'/feedbacks').
 then(response => response.json()).
 then(data => {
     document.querySelector(".spinner").remove();
@@ -91,3 +154,11 @@ then(data => {
 }).catch(error => {
     feedEl.textContent = `Failed to fetch items. ${error.message}`;
 });
+//#endregion
+
+//#region Events
+textAreaEl.addEventListener('input', InputHandler);
+formEl.addEventListener('submit', SubmitHandler);
+feedEl.addEventListener('click', ClickHandler);
+hashtagListEl.addEventListener('click', HashtagClickHandler);
+//#endregion
